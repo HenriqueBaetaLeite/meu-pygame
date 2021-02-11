df6c3ca4fb6a6b1da791774188943ef4367883e2
henrique <hbaetaleite@gmail.com>
2021-02-11 16:51:54 -0300

    melhorando o README

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 <h2>Meu jogo Python 2d utilizando a lib <a href="https://www.pygame.org/news" target="_blank" rel="noreferrer noopener">pygame</a></h2>
 <p>Aprendizados reunidos durante o curso da Trybe e meus estudos autodidatas...</p>
 
-<img src="https://github.com/HenriqueBaetaLeite/meu-pygame/blob/main/rascunho1.png" alt="rascunhos pygame" />
+<p>Para executar o jogo, faça o git clone do repositório,</p>
+<p>certifique-se de ter instalado o Python 3 em sua máquina,</p>
+<p>crie um ambiente virtual e execute o comando abaixo para instalar as dependências:</p>
+<span>-- Apenas a lib pygame é utilizada --</span>
+<p>>> python3 -m pip install -r requirements.txt</p>
 
+<img src="https://github.com/HenriqueBaetaLeite/meu-pygame/blob/main/rascunho1.png" alt="rascunhos pygame" />
--- create_timer.py
+++ create_timer.py
@@ -1,4 +1,5 @@
 # import pygame
+import time
 
 # fonte = pygame.font.SysFont("likhan", 30, bold=True, italic=True)
 
@@ -8,7 +9,30 @@
 # screen = pygame.display.set_mode((width, height))
 
 
-class Timer:
-    def start_timer():
-    #     timer_formated = fonte.render("Tempo: 00:30", True, (0, 0, 0))
-    #     screen.blit(timer_formated, (10, 10))
+# class CrateTimer:
+#     def set_timer():
+#         t = 30
+#         while t:
+#             mins, secs = divmod(t, 60)
+#             timer = "{:02d}:{:02d}".format(mins, secs)
+#             print(timer, end="\r")
+#             time.sleep(1)
+#             t -= 1
+
+#         print("Fire in the hole!!")
+
+
+def set_timer(t):
+    # t = 15
+    while t:
+        mins, secs = divmod(t, 60)
+        # timer = f'{mins}:{secs}'
+        timer = "{:02d}:{:02d}".format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+
+    print("Game Over!!")
+
+
+set_timer(25)
